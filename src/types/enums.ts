d07311ef9105c01f482d8d37e65729642e400481
moshangssh <siniansiyin@163.com>
2025-02-25 11:47:24 +0800

    删除未使用的类型定义和已弃用的文件： - 删除 `markdown-proc.ts`，这是一个未使用且已弃用的文件 - 移除 `file-processor-factory.ts`，一个尚未实现的工厂类 - 除包含未使用的 `ValidationResult` 枚举类型的 `enums.ts` 文件

--- src/types/enums.ts
+++
@@ -1,7 +0,0 @@
-export enum ValidationResult {
-    SUCCESS = 'success',
-    INVALID_REGEX = 'invalid_regex',
-    INVALID_GROUP = 'invalid_group',
-    INVALID_NUMBER = 'invalid_number',
-    INVALID_FOLDER = 'invalid_folder'
-}
